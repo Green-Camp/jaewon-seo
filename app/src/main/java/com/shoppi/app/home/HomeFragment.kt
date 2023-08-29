92f132e1cd6e64559f98f100b5e49628eac08aaf
librarywon <librarywon@naver.com>
2023-08-30 01:17:54 +0900

    [feat]: assetLoader 제작

--- app/src/main/java/com/shoppi/app/home/HomeFragment.kt
+++ app/src/main/java/com/shoppi/app/home/HomeFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.shoppi.app.R
+import com.shoppi.app.util.logD
 
 class HomeFragment : Fragment() {
     override fun onCreateView(
@@ -22,6 +23,11 @@ class HomeFragment : Fragment() {
         val button = view.findViewById<View>(R.id.btn_product_detail)
         button.setOnClickListener {
             findNavController().navigate(R.id.action_navigation_home_to_navigation_product_detail)
+        }
+
+        val assetLoader = AssetLoader()
+        assetLoader.getJsonString(requireContext(), "home.json")?.let {
+            logD(it)
         }
     }
 }
