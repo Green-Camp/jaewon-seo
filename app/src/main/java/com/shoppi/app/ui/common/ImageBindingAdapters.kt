aeb15be95170734be83177cb4d59ffb2d2310843
librarywon <librarywon@naver.com>
2023-09-20 11:44:08 +0900

    [feat]: 중첩 rv layout 구현

--- app/src/main/java/com/shoppi/app/ui/common/ImageBindingAdapters.kt
+++ app/src/main/java/com/shoppi/app/ui/common/ImageBindingAdapters.kt
@@ -3,9 +3,18 @@ package com.shoppi.app.ui.common
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import coil.load
+import coil.transform.CircleCropTransformation
 
 @BindingAdapter("imageUrl")
 fun loadImage(view: ImageView, imageUrl: String?) {
     if (!imageUrl.isNullOrEmpty())
         view.load(imageUrl)
+}
+
+@BindingAdapter("circleImageUrl")
+fun loadCircleImage(view: ImageView, imageUrl: String?) {
+    if (!imageUrl.isNullOrEmpty())
+        view.load(imageUrl) {
+            transformations(CircleCropTransformation())
+        }
 }
